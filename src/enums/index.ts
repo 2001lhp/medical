0ebad46dd3716c911da7b6b490fe97e34fba1a34
lhp <2370033560@qq.cpm>
2023-09-10 21:31:42 +0800

    feat(room): 问诊室

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -41,3 +41,34 @@ export enum ConsultFlag {
     isFlag = 1,
     noFlag = 0
 }
+
+export enum MsgType {
+    /** 文字聊天 */
+    MsgText = 1,
+    /** 图片聊天 */
+    MsgImage = 4,
+    /** 患者信息 */
+    CardPat = 21,
+    /** 处方信息 */
+    CardPre = 22,
+    /** 未评价信息 */
+    CardEvaForm = 23,
+    /** 已评价信息 */
+    CardEva = 24,
+    /** 通用通知 */
+    Notify = 31,
+    /** 温馨提示 */
+    NotifyTip = 32,
+    /** 取消提示 */
+    NotifyCancel = 33
+}
+
+// 处方状态
+export enum PrescriptionStatus {
+    /** 未付款 */
+    NotPayment = 1,
+    /** 已付款 */
+    Payment = 2,
+    /** 已失效 */
+    Invalid = 3
+}
--- src/views/Room/index.vue
+++ src/views/Room/index.vue
@@ -1,12 +1,77 @@
-<script setup lang='ts'></script>
+<script setup lang="ts">
+import { io } from 'socket.io-client'
+import { onMounted, ref } from 'vue'
+import { useCounterStore } from '@/stores/counter'
+import { useRoute } from 'vue-router'
+import type { Message, TimeMessages } from '@/types/room'
+import { MsgType } from '@/enums'
+const store = useCounterStore()
+const route = useRoute()
+
+const list = ref<Message[]>()
+
+onMounted(async () => {
+    // 连接websocket
+    const socket = io('https://consult-api.itheima.net/', {
+        auth: {
+            token: 'Bearer ' + store.user?.token
+        },
+        query: {
+            orderId: route.query.orderId
+        }
+    })
+
+    // 监听是否连接成功
+    socket.on('connect', () => {
+        console.log('连接成功')
+    })
+
+    // 监听是否断开链接
+    socket.on('disconnect', () => {
+        console.log('断开链接')
+    })
+
+    // 监听是否连接失败
+    socket.on('error', (event) => {
+        console.log('连接失败', event)
+    })
+
+    // 监听默认的聊天信息
+    socket.on('chatMsgList', ({ data }: { data: TimeMessages[] }) => {
+        const arr: Message[] = []
+
+        console.log('e', data)
+        data.forEach((item) => {
+            arr.push({
+                msgType: MsgType.Notify,
+                createTime: item.createTime,
+                id: item.createTime,
+                msg: { content: item.createTime }
+            })
+            arr.push(...item.items)
+        })
+        console.log('arr', arr)
+        list.value = arr
+    })
+})
+</script>
+
 <template>
-    <div class='room'>
+    <div class="room">
         <nav-bar title="医生问诊室"/>
-        <van-button @click="$router.push('/order/pay?id=6938549059629056')">药品订单</van-button>
+        <room-status/>
+        <room-message :list="list"/>
+        <room-action/>
+        <!--<van-button @click="$router.push('/order/pay?id=6938560516042752')">购买药品</van-button> -->
     </div>
 </template>
-<style lang='scss' scoped>
-.room{
-    padding-top: 46px;
+
+<style lang="scss" scoped>
+.room {
+    padding-top: 24vw;
+    padding-bottom: 16vw;
+    min-height: 100vh;
+    box-sizing: border-box;
+    background-color: var(--cp-bg);
 }
 </style>
